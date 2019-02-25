f9605d399129371c2413a615eca316c4df0702fd
Tapio Malmberg <tapio.malmberg@gmail.com>
2019-02-25 13:28:36 +0200

    Fix mouse wheel scrolling.

---
+++ JLyricsUtil/IgnoreMouseWheelBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Input;
+
+namespace JLyricsUtil
+{
+    /// <summary>
+    /// Captures and eats MouseWheel events so that a nested ListBox does not
+    /// prevent an outer scrollable control from scrolling.
+    /// </summary>
+    public sealed class IgnoreMouseWheelBehavior : Behavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseWheel += AssociatedObject_PreviewMouseWheel;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewMouseWheel -= AssociatedObject_PreviewMouseWheel;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+
+            var e2 = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+            {
+                RoutedEvent = UIElement.MouseWheelEvent
+            };
+
+            AssociatedObject.RaiseEvent(e2);
+        }
+    }
+}
